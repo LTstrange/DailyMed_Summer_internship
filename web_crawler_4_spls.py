c31ebbcd99a944ed823bd6f3fc80e34139ab2fab
LTstrange <835422774@qq.com>
2020-04-17 15:09:56 +0800

    丰富了爬取SPL的程序的注释，并整理了代码。

--- web_crawler_4_spls.py
+++ web_crawler_4_spls.py
@@ -1,52 +1,48 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/17 15:09
+# @Author  : LTstrange
+
 import os
 import traceback
-import urllib.error
 from urllib.request import urlopen
 import re
 from multiprocessing import Pool
-import socket
-import time
 
 from tqdm import tqdm
 
+# 用来匹配setID的正则表达式
 setID_comp = re.compile(r'(?<=\')[a-zA-Z0-9\-]+?(?=\')')
 
+# 下载setID对应的说明书（spl）的网址
 url = 'https://dailymed.nlm.nih.gov/dailymed/services/v2/spls/{setID}.xml'
 
 
+# 根据setID下载SPL说明书
 def download_SPL(setID):
     content = ''
+    # 尝试连接，并设置为超过10秒就断开连接
     try:
         for line in urlopen(url.format(setID=setID), timeout=10):
             line = line.decode('utf-8')
             content += line
-    # except urllib.error.HTTPError as e:
-    #     print(e)
-    #     return
-    # except urllib.error.URLError as e:
-    #     print('{setID} TIMEOUT!!!'.format(setID=setID))
-    #     return
-    # except socket.timeout as e:
-    #     print('{setID} TIMEOUT!!!'.format(setID=setID))
-    #     return
-    # except ConnectionResetError as e:
-    #     print(e)
-    #     return
+    # 只要出错，就暂时放弃这个连接，等待下次手动重启时，再重新下载
     except:
         print(traceback.format_exc().split('\n')[-2])
         return
-    # print('saving {setID}...'.format(setID=setID))
+    # 每下载完毕一个SPL说明书，就进行保存
     with open('spls/{setID}.xml'.format(setID=setID), 'w', encoding='utf-8') as file:
         file.write(content)
 
 
 if __name__ == '__main__':
+    # 整理所有需要下载的setID
     print('getting setIDs...')
     setIDs = []
     with open('setIDs.txt', 'r') as file:
         content = file.read()
         setIDs = re.findall(setID_comp, content)
 
+    # 排除掉已经下载过的setID
     files = os.listdir('spls/')
     print('files len:', len(files))
     print('setIDs len:(before)', len(setIDs))
@@ -59,6 +55,7 @@ if __name__ == '__main__':
             exit()
     print('setIDs len:(after)', len(setIDs))
 
+    # 开启多线程，并下载对应SPL说明书
     with Pool(60) as p:
         p.map(download_SPL, setIDs)
 
