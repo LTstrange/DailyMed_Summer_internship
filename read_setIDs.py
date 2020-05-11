77872c0fe780a6fc00261f84b7fb14d1a8da6205
LTstrange <835422774@qq.com>
2020-05-11 19:46:03 +0800

    完善补充read_setIDs.py的程序注释。

--- read_setIDs.py
+++ read_setIDs.py
@@ -1,12 +1,15 @@
 import re
 import os
 
+# 用于匹配文件内setID的正则表达式
 setID_comp = re.compile(r'(?<=\')[a-zA-Z0-9\-]+?(?=\')')
 
+# 遍历给定目录下的所有文件名
 files = os.listdir('setIDs')
 
 all_setIDs = set()
 
+# 遍历给定的文件夹，汇总出已经下载的setIDs
 for file in files:
     with open('setIDs/' + file, 'r') as file:
         content = file.read()
@@ -15,6 +18,7 @@ for file in files:
 
 print(len(all_setIDs))
 
+# 创建setIDs.txt文件并保存
 with open('setIDs.txt', 'w') as file:
     file.write(str(all_setIDs))
 
