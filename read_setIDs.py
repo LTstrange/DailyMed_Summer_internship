32ec0a9064e94ba789395502e42a5f0cdd616700
LTstrange <835422774@qq.com>
2020-07-04 16:25:50 +0800

    修复了在read_setIDs.py中，因文件地址与运行地址不统一导致的文件保存错误。

--- read_setIDs.py
+++ read_setIDs.py
@@ -8,27 +8,30 @@ import os
 # 用于匹配文件内setID的正则表达式
 setID_comp = re.compile(r'(?<=\')[a-zA-Z0-9\-]+?(?=\')')
 
+# 设置相对于文件的存储位置
 file_dir = os.path.dirname(os.path.abspath(__file__))
-if not os.path.exists(file_dir+r'\setIDs.txt'):
+storge_dir = file_dir + r'\setIDs'
+
+if not os.path.exists(file_dir+r'\setIDs'):
     print("Please run 'web_crawler_4_setIDs.py' first. Then run this program.")
     exit()
 
 # 遍历给定目录下的所有文件名
-files = os.listdir('setIDs')
+files = os.listdir(storge_dir)
 
 all_setIDs = set()
 
 # 遍历给定的文件夹，汇总出已经下载的setIDs
 for file in files:
-    with open('setIDs/' + file, 'r') as file:
+    with open(os.path.join(storge_dir, file), 'r') as file:
         content = file.read()
         setIDs = re.findall(setID_comp, content)
         all_setIDs.update(setIDs)
 
-print(len(all_setIDs))
+print(f'Sorted out {len(all_setIDs)} setIDs')
 
 # 创建setIDs.txt文件并保存
-with open('setIDs.txt', 'w') as file:
+with open(file_dir+r'\setIDs.txt', 'w') as file:
     file.write(str(all_setIDs))
 
 
